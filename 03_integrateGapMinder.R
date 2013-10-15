6de51d7ac0162b01bb900afc7ce1a80fe3ceee3b
Dean Attali <daattali@gmail.com>
2013-10-15 01:42:57 -0700

    add code to the gap minder script

--- 03_integrateGapMinder.R
+++ 03_integrateGapMinder.R
@@ -1,2 +1,48 @@
 source('common.R')
 
+
+countriesAttacks <- ddply(dat, ~country+region, function(x) { return(data.frame(totAttacks = nrow(x))) })
+head(arrange(countriesAttacks, totAttacks,decreasing=TRUE),n=15)
+
+gDat <- read.delim("gapminderDataFiveYear.txt")
+str(gDat)
+countriesAttacks2<-merge(countriesAttacks, subset(gDat, year==max(year), select=c('country','pop')), by.x = 'country', by.y='country')
+countriesAttacks2$attacksNorm <- countriesAttacks2$pop / countriesAttacks2$totAttacks
+
+
+
+
+#gdp
+nn<-merge(countriesAttacks, subset(gDat, year==max(year)), by.x=c('country'),by.y=c('country'))
+xyplot(gdpPercap~totAttacks,nn)
+# usually poorer countries get terrorist activities
+
+nn<-merge(countriesAttacks3, gDat, by.x=c('country','year'),by.y=c('country','year'))
+nn<-nn[-which(nn$gdpPercap==max(nn$gdpPercap)),]
+xyplot(gdpPercap~totAttacks|year,nn,group=continent,pch=18,auto.key=TRUE)
+
+
+countriesAttacktype <-ddply(dat, .(country, attacktype1), function(x){data.frame(sum(x$nkill,na.rm=TRUE))})
+names(countriesAttacktype)[1]<-'country'
+names(countriesAttacktype)[3]<-'nkill'
+vvv<-merge(countriesAttacktype,subset(gDat, year==2007,select=c('country','gdpPercap')))
+xyplot(nkill~gdpPercap, vvv, group=attacktype1,auto.key=TRUE,pch=18,cex=1)
+# can see USA cuz of 9/11
+
+deadliest <- tail(arrange(subset(dat, !is.na(nkill)),nkill), n=250)
+deadliest2<-subset(deadliest,select=c('year','imonth','iday','attacktype1','nkill','nwound','country'))
+names(deadliest2)[7]<-'country'
+bb<-merge(deadliest2,subset(gDat, year==2007,select=c('country','gdpPercap')))
+xyplot(nkill~gdpPercap, bb, group=attacktype1,auto.key=TRUE,pch=18,cex=1)
+# can see attacks in the US are outliers (1995 oklahoma bombing and 9/11), can also see canada 1985 and rwanda assault
+
+# MAKE COPY OF DAT FIRST, do following opeartions on the copy
+dat[is.na(dat$nkill),]$nkill = 0               
+dat[is.na(dat$nwound),]$nwound = 0
+woundest <- tail(arrange(dat, nwound+nkill), n=250)
+woundest$tot<-woundest$nkill+woundest$nwound
+woundest<-subset(woundest,select=c('year','imonth','iday','attacktype1','nkill','nwound','country','tot'))
+woundestG<-merge(woundest,subset(gDat, year==2007,select=c('country','gdpPercap','continent')), by.x='country',by.y='country')
+xyplot(tot~gdpPercap, woundestG, group=attacktype1,auto.key=TRUE,pch=18,cex=1)
+# or arrange by continent too
+# i think the 10000 wounded in peru in 1983 is an error. i tried looking it up, couldn't find any info about it, but did happen in sri lanka.
